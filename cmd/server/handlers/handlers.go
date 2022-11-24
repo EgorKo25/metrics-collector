03a08f44d6c69487af5ee74d36bb4c47b817731a
Egor <egorko25@gmail.com>
2022-11-24 20:48:23 +0300

    1. added error procces 2. added statusCode 501 404

--- cmd/server/handlers/handlers.go
+++ cmd/server/handlers/handlers.go
@@ -1,6 +1,7 @@
-package server
+package handlers
 
 import (
+	"log"
 	"net/http"
 	"strconv"
 	"strings"
@@ -15,27 +16,39 @@ func GetMetricList(MetricList *map[string]Guage, CounterList *map[string]Counter
 		data := strings.Split(url, "/")
 
 		if data[2] == "guage" {
-			value, err := strconv.ParseFloat(data[4], 64)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-			}
-			(*MetricList)[data[3]] = Guage(value)
+			if len(data) > 4 {
+				value, err := strconv.ParseFloat(data[4], 64)
+				if err != nil {
+					w.WriteHeader(http.StatusInternalServerError)
+				}
+				(*MetricList)[data[3]] = Guage(value)
+				log.Println(MetricList)
 
-			w.WriteHeader(http.StatusOK)
+				w.WriteHeader(http.StatusOK)
+				return
+			}
+
+			w.WriteHeader(http.StatusNotFound)
 			return
 
 		}
 		if data[2] == "counter" {
-			value, err := strconv.Atoi(data[4])
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-			}
-			(*CounterList)[data[3]] = Counter(value)
+			if len(data) > 4 {
+				value, err := strconv.Atoi(data[4])
+				if err != nil {
+					w.WriteHeader(http.StatusInternalServerError)
+				}
+				(*CounterList)[data[3]] = Counter(value)
+				log.Println(CounterList)
 
-			w.WriteHeader(http.StatusOK)
+				w.WriteHeader(http.StatusOK)
+				return
+			}
+			w.WriteHeader(http.StatusNotFound)
 			return
+
 		}
-		w.WriteHeader(http.StatusBadRequest)
+		w.WriteHeader(http.StatusNotImplemented)
 		return
 	}
 }
