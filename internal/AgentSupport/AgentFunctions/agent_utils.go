f379df12adf73dabacb976be0d1a85f4f99a53f1
Egor <egorko25@gmail.com>
2022-12-02 23:18:52 +0300

    Empty commit

--- internal/AgentSupport/AgentFunctions/agent_utils.go
+++ internal/AgentSupport/AgentFunctions/agent_utils.go
@@ -1,14 +1,15 @@
 package AgentFunctions
 
 import (
+	"DevOps-Track-Yandex/internal/StorageSupport"
+	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"net/url"
 	rt "runtime"
-	"strconv"
 	"time"
 )
-
-type counter int
 
 type config struct {
 	pollInterval   time.Duration
@@ -16,8 +17,8 @@ type config struct {
 }
 
 // This function makes a copy of the map so that the metrics can be updated.
-func copyMap(metricList *map[string]*uint64) map[string]uint64 {
-	oldMetricList := make(map[string]uint64)
+func copyMap(metricList *map[string]*StorageSupport.Gauge) map[string]StorageSupport.Gauge {
+	oldMetricList := make(map[string]StorageSupport.Gauge)
 	for k, v := range *metricList {
 		oldMetricList[k] = *v
 	}
@@ -25,8 +26,9 @@ func copyMap(metricList *map[string]*uint64) map[string]uint64 {
 }
 
 // updating and counting updated metrics
-func updateAndCount(m *rt.MemStats, metricList *map[string]*uint64, pollCounter *counter) {
+func updateAndCount(m *rt.MemStats, metricList *map[string]*StorageSupport.Gauge, pollCounter *StorageSupport.Counter) {
 
+	(*metricList)["RandomValue"] = metricPtrCreator(StorageSupport.Gauge(rand.Float64()))
 	oldMetricList := copyMap(metricList)
 	//Updating
 	rt.ReadMemStats(m)
@@ -37,47 +39,71 @@ func updateAndCount(m *rt.MemStats, metricList *map[string]*uint64, pollCounter 
 		}
 	}
 }
+func metricPtrCreator(val StorageSupport.Gauge) *StorageSupport.Gauge {
+	return &val
+}
 
 // function to create a metrics list and Memory Stats obj
-func createMetricList(pollCounter *counter) (metricList map[string]*uint64) {
+func createMetricList(pollCounter *StorageSupport.Counter) (metricList map[string]*StorageSupport.Gauge) {
 	var m rt.MemStats
 	rt.ReadMemStats(&m)
 
-	GCCPUFraction := uint64(m.GCCPUFraction)
-	metricList = map[string]*uint64{
-		"Alloc":         &m.Alloc,
-		"BuckHashSys":   &m.BuckHashSys,
-		"Frees":         &m.Frees,
-		"GCCPUFraction": &GCCPUFraction,
-		"GCSys":         &m.GCSys,
-		"HeapAlloc":     &m.HeapAlloc,
-		"HeapIdle":      &m.HeapIdle,
-	}
+	metricList = make(map[string]*StorageSupport.Gauge)
+	metricList["Alloc"] = metricPtrCreator(StorageSupport.Gauge(m.Alloc))
+	metricList["BuckHashSys"] = metricPtrCreator(StorageSupport.Gauge(m.BuckHashSys))
+	metricList["Frees"] = metricPtrCreator(StorageSupport.Gauge(m.Frees))
+	metricList["GCCPUFraction"] = metricPtrCreator(StorageSupport.Gauge(m.GCCPUFraction))
+	metricList["GCSys"] = metricPtrCreator(StorageSupport.Gauge(m.GCSys))
+	metricList["HeapAlloc"] = metricPtrCreator(StorageSupport.Gauge(m.HeapAlloc))
+	metricList["HeapIdle"] = metricPtrCreator(StorageSupport.Gauge(m.HeapIdle))
+	metricList["HeapInuse"] = metricPtrCreator(StorageSupport.Gauge(m.HeapInuse))
+	metricList["HeapObjects"] = metricPtrCreator(StorageSupport.Gauge(m.HeapObjects))
+	metricList["HeapReleased"] = metricPtrCreator(StorageSupport.Gauge(m.HeapReleased))
+	metricList["HeapSys"] = metricPtrCreator(StorageSupport.Gauge(m.HeapSys))
+	metricList["LastGC"] = metricPtrCreator(StorageSupport.Gauge(m.LastGC))
+	metricList["Lookups"] = metricPtrCreator(StorageSupport.Gauge(m.Lookups))
+	metricList["MCacheInuse"] = metricPtrCreator(StorageSupport.Gauge(m.MCacheInuse))
+	metricList["MCacheSys"] = metricPtrCreator(StorageSupport.Gauge(m.MCacheSys))
+	metricList["MSpanInuse"] = metricPtrCreator(StorageSupport.Gauge(m.MSpanInuse))
+	metricList["MSpanSys"] = metricPtrCreator(StorageSupport.Gauge(m.MSpanSys))
+	metricList["Mallocs"] = metricPtrCreator(StorageSupport.Gauge(m.Mallocs))
+	metricList["NextGC"] = metricPtrCreator(StorageSupport.Gauge(m.NextGC))
+	metricList["NumForcedGC"] = metricPtrCreator(StorageSupport.Gauge(m.NumForcedGC))
+	metricList["NumGC"] = metricPtrCreator(StorageSupport.Gauge(m.NumGC))
+	metricList["OtherSys"] = metricPtrCreator(StorageSupport.Gauge(m.OtherSys))
+	metricList["PauseTotalNs"] = metricPtrCreator(StorageSupport.Gauge(m.PauseTotalNs))
+	metricList["StackInuse"] = metricPtrCreator(StorageSupport.Gauge(m.StackInuse))
+	metricList["StackSys"] = metricPtrCreator(StorageSupport.Gauge(m.StackSys))
+	metricList["Sys"] = metricPtrCreator(StorageSupport.Gauge(m.Sys))
+	metricList["TotalAlloc"] = metricPtrCreator(StorageSupport.Gauge(m.TotalAlloc))
+	metricList["RandomValue"] = metricPtrCreator(StorageSupport.Gauge(rand.Float64()))
 
 	updateAndCount(&m, &metricList, pollCounter)
 	return
 }
 
 // sending collected metrics to 127.0.0.1/updates on port 8080
-func sendData(metricsList *map[string]*uint64) {
+func sendData(metricsList *map[string]*StorageSupport.Gauge) {
 	for k, v := range *metricsList {
+		mType := "gauge"
+		URL, err := url.JoinPath("http://127.0.0.1:8080", "update", mType, k, fmt.Sprintf("%f", *v))
+		if err != nil {
+			log.Fatalln(err)
+		}
 
-		url := "http://127.0.0.1:8080/update/" + "type" + "/" + k + "/" + strconv.Itoa(int(*v)) + "/"
-		_, err := http.Post(url, "text/plain", nil)
+		_, err = http.Post(URL, "text/plain", nil)
 		if err != nil {
 			log.Fatal(err)
 		}
-
 	}
-
 }
 
 // system monitor
 func MyMonitor() {
 	//metric counter
-	var pollCount counter = 0
+	var pollCount StorageSupport.Counter = 0
 	//map for metric
-	var metricsList map[string]*uint64
+	var metricsList map[string]*StorageSupport.Gauge
 
 	//interval settings
 	var durConf config
---
+++ internal/StorageSupport/MemStatRule.go
@@ -0,0 +1,45 @@
+package StorageSupport
+
+type Gauge float64
+type Counter uint64
+
+type MemStats struct {
+	MetricsGauge   map[string]Gauge
+	MetricsCounter map[string]Counter
+}
+
+func (m MemStats) GetStats(name string, value any, mType string) {
+	if mType == "gauge" {
+		m.MetricsGauge[name] = value.(Gauge)
+	}
+	if mType == "counter" {
+		m.MetricsCounter[name] = value.(Counter)
+	}
+
+}
+func (m MemStats) TakeStats() (map[string]Gauge, map[string]Counter) {
+	return m.MetricsGauge, m.MetricsCounter
+}
+func (m MemStats) TakeThisStat(name, mType string) (value any) {
+	if mType == "gauge" {
+		value = m.MetricsGauge[name]
+		return value
+	}
+	if mType == "counter" {
+		value = m.MetricsCounter[name]
+		return value
+	}
+
+	return nil
+}
+func (m *MemStats) CreateBaseMap() {
+	m.MetricsCounter = make(map[string]Counter)
+	m.MetricsGauge = make(map[string]Gauge)
+}
+
+type MemStatsRule interface {
+	GetStats(map[string]any)
+	TakeStats() (map[string]Gauge, map[string]Counter)
+	TakeThisStat(string) any
+	CreateBaseMap()
+}
