0b9abcb67a70d56b8f07665807a1fc0fd9c696f5
Egor <egorko25@gmail.com>
2022-12-02 22:53:39 +0300

    Create chi router tests

--- internal/ServerSupport/routers/Routers_test.go
+++ internal/ServerSupport/routers/Routers_test.go
@@ -1,1 +1,49 @@
 package routers
+
+import (
+	"DevOps-Track-Yandex/internal/StorageSupport"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestNewRouter(t *testing.T) {
+	var m StorageSupport.MemStats
+	m.CreateBaseMap()
+
+	r := NewRouter(m)
+	ts := httptest.NewServer(r)
+	defer ts.Close()
+
+	m.GetStats("Alloc", any(StorageSupport.Gauge(123)), "gauge")
+
+	statusCode, body := testRequest(t, ts, "GET", "/")
+	assert.Equal(t, http.StatusOK, statusCode)
+	assert.Equal(t, "> Alloc:  123.000000\n", body)
+
+	statusCode, body = testRequest(t, ts, "GET", "/update/gauge/Alloc")
+	assert.Equal(t, http.StatusOK, statusCode)
+	assert.Equal(t, "123\n", body)
+
+	statusCode, body = testRequest(t, ts, "POST", "/update/gauge/Alloc/123")
+	assert.Equal(t, http.StatusOK, statusCode)
+	assert.Equal(t, "", body)
+
+}
+func testRequest(t *testing.T, ts *httptest.Server, method, path string) (int, string) {
+	req, err := http.NewRequest(method, ts.URL+path, nil)
+	require.NoError(t, err)
+
+	resp, err := http.DefaultClient.Do(req)
+	require.NoError(t, err)
+
+	respBody, err := io.ReadAll(resp.Body)
+	require.NoError(t, err)
+
+	defer resp.Body.Close()
+
+	return resp.StatusCode, string(respBody)
+}
