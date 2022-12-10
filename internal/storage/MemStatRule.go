1c79ebcfe56eb50d3058db2161adc077aba0fc00
Egor <egorko25@gmail.com>
2022-12-10 03:09:03 +0300

    1.Интерфейс поднять в самый верх 2. Изменено имя структуры, для того чтобы не совпадало с именем в пакете runtime 3. Во все методы теперь передатся ссылка на структуру 4. Создан конструктор структуры 5. Интерфейс переименован

--- internal/storage/MemStatRule.go
+++ internal/storage/MemStatRule.go
@@ -3,35 +3,41 @@ package storage
 type Gauge float64
 type Counter uint64
 
-type MemStats struct {
+type Storagier interface {
+	GetStats(map[string]any)
+	TakeStats() (map[string]Gauge, map[string]Counter)
+	TakeThisStat(string) any
+}
+
+type MetricStorage struct {
 	MetricsGauge   map[string]Gauge
 	MetricsCounter map[string]Counter
 }
 
-func (m MemStats) GetStats(name string, value any, mType string) {
+func (m *MetricStorage) GetStats(name string, value any, mType string) {
 	if mType == "gauge" {
-		m.MetricsGauge[name] = value.(Gauge)
+		(*m).MetricsGauge[name] = value.(Gauge)
 	}
 	if mType == "counter" {
-		m.MetricsCounter[name] += value.(Counter)
+		(*m).MetricsCounter[name] += value.(Counter)
 
 	}
 
 }
-func (m MemStats) TakeStats() (map[string]Gauge, map[string]Counter) {
-	return m.MetricsGauge, m.MetricsCounter
+func (m *MetricStorage) TakeStats() (map[string]Gauge, map[string]Counter) {
+	return (*m).MetricsGauge, (*m).MetricsCounter
 }
-func (m MemStats) TakeThisStat(name, mType string) (value any) {
+func (m *MetricStorage) TakeThisStat(name, mType string) (value any) {
 	if mType == "gauge" {
-		if _, ok := m.MetricsGauge[name]; ok {
-			value = m.MetricsGauge[name]
+		if _, ok := (*m).MetricsGauge[name]; ok {
+			value = (*m).MetricsGauge[name]
 			return value
 		}
 		return nil
 	}
 	if mType == "counter" {
-		if _, ok := m.MetricsCounter[name]; ok {
-			value = m.MetricsCounter[name]
+		if _, ok := (*m).MetricsCounter[name]; ok {
+			value = (*m).MetricsCounter[name]
 			return value
 
 		}
@@ -40,16 +46,9 @@ func (m MemStats) TakeThisStat(name, mType string) (value any) {
 
 	return nil
 }
-func CreateBaseStorage() *MemStats {
-	var m MemStats
+func NewStorage() *MetricStorage {
+	var m MetricStorage
 	m.MetricsCounter = make(map[string]Counter)
 	m.MetricsGauge = make(map[string]Gauge)
 	return &m
-}
-
-type MemStatsRule interface {
-	GetStats(map[string]any)
-	TakeStats() (map[string]Gauge, map[string]Counter)
-	TakeThisStat(string) any
-	CreateBaseStorage() *MemStats
 }
