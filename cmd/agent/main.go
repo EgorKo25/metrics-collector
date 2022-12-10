eddd151f051eb658b4c87a0bbe2f11bee0d77c35
Egor <egorko25@gmail.com>
2022-12-10 03:06:36 +0300

    1. A config is created 2. The monitor object is created 3. The Run() method is started

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"github.com/EgorKo25/DevOps-Track-Yandex/internal/agent"
+	config "github.com/EgorKo25/DevOps-Track-Yandex/internal/configuration"
 )
 
 func main() {
-	agent.MyMonitor()
+	//Формирую конфигурацию монитора
+	cfg := config.NewAgentConfig(2, 10)
+	//Создаю новый монитор
+	monitor := agent.NewMonitor(cfg)
+	//Запускаю монитор
+	go monitor.Run()
 }
