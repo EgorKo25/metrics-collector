e8041945444d41edfc9747365314add115929615
Egor <egorko25@gmail.com>
2022-12-17 20:08:53 +0300

    1. Enter the handler struct 2. Rename storage --handlers.go-- 3. Rename func 4. Added go dock

--- internal/server/handlers/handlers.go
+++ internal/server/handlers/handlers.go
@@ -7,114 +7,90 @@ import (
 	"log"
 	"net/http"
 	"strconv"
-	"strings"
 )
 
-func ShowThisMetricValue(m *storage.MetricStorage) func(w http.ResponseWriter, r *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		res := (*m).TakeThisStat(chi.URLParam(r, "name"), chi.URLParam(r, "type"))
-		if res == nil {
-			w.WriteHeader(http.StatusNotFound)
-			return
-		}
-		_, err := w.Write([]byte(fmt.Sprintf("%v\n", res)))
+type Handler struct {
+	storage *storage.MetricStorage
+}
+
+// NewHandler handler type constructor
+func NewHandler(storage *storage.MetricStorage) *Handler {
+	return &Handler{
+		storage: storage,
+	}
+}
+
+// GetValueStat a handler that returns the value of a specific metric
+func (h Handler) GetValueStat(w http.ResponseWriter, r *http.Request) {
+	res := h.storage.StatStatus(chi.URLParam(r, "name"), chi.URLParam(r, "type"))
+	if res == nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	_, err := w.Write([]byte(fmt.Sprintf("%v\n", res)))
+	if err != nil {
+		log.Printf("Cannot write reqeust: %s", err)
+	}
+	w.WriteHeader(http.StatusOK)
+	return
+
+}
+
+// GetAllStats returns the values of all metrics
+func (h Handler) GetAllStats(w http.ResponseWriter, _ *http.Request) {
+	w.WriteHeader(http.StatusOK)
+
+	for k, v := range h.storage.MetricsGauge {
+
+		tmp := "> " + k + ":  " + fmt.Sprintf("%f", v) + "\n"
+
+		_, err := w.Write([]byte(tmp))
 		if err != nil {
-			log.Printf("Cannot write reqeust: %s", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Fatalln(err)
 		}
+	}
+
+	for k, v := range h.storage.MetricsCounter {
+
+		tmp := "> " + k + ":  " + fmt.Sprintf("%d", v) + "\n"
+
+		_, err := w.Write([]byte(tmp))
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Fatalln(err)
+		}
+	}
+}
+
+// SetMetricValue sets the value of the specified metric
+func (h Handler) SetMetricValue(w http.ResponseWriter, r *http.Request) {
+
+	if mType := chi.URLParam(r, "type"); mType == "gauge" {
+
+		value, err := strconv.ParseFloat(chi.URLParam(r, "value"), 64)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			log.Printf("Somethings went wrong: %s", err)
+			return
+		}
+
+		h.storage.SetGaugeStat(chi.URLParam(r, "name"), storage.Gauge(value), mType)
 		w.WriteHeader(http.StatusOK)
-		return
-
 	}
-}
-func ShowAllMetricFromStorage(m *storage.MetricStorage) func(w http.ResponseWriter, r *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
+	if mType := chi.URLParam(r, "type"); mType == "counter" {
+
+		value, err := strconv.Atoi(chi.URLParam(r, "value"))
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			log.Printf("Somethings went wrong: %s", err)
+			return
+		}
+
+		h.storage.SetCounterStat(chi.URLParam(r, "name"), storage.Counter(value), mType)
 		w.WriteHeader(http.StatusOK)
-		for k, v := range (*m).MetricsGauge {
-			tmp := "> " + k + ":  " + fmt.Sprintf("%f", v) + "\n"
-			_, err := w.Write([]byte(tmp))
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				log.Fatalln(err)
-			}
-		}
-		for k, v := range (*m).MetricsCounter {
-			tmp := "> " + k + ":  " + fmt.Sprintf("%d", v) + "\n"
-			_, err := w.Write([]byte(tmp))
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				log.Fatalln(err)
-			}
-		}
 	}
-}
-func AddMetricToStorage(m *storage.MetricStorage) func(w http.ResponseWriter, r *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		if mType := chi.URLParam(r, "type"); mType == "gauge" {
-			value, err := strconv.ParseFloat(chi.URLParam(r, "value"), 64)
-			if err != nil {
-				w.WriteHeader(http.StatusBadRequest)
-				log.Printf("Somethings went wrong: %s", err)
-				return
-			}
-			(*m).GetStats(chi.URLParam(r, "name"), any(storage.Gauge(value)), mType)
-			w.WriteHeader(http.StatusOK)
-		}
-		if mType := chi.URLParam(r, "type"); mType == "counter" {
-			value, err := strconv.Atoi(chi.URLParam(r, "value"))
-			if err != nil {
-				w.WriteHeader(http.StatusBadRequest)
-				log.Printf("Somethings went wrong: %s", err)
-				return
-			}
+	w.WriteHeader(http.StatusNotImplemented)
+	return
 
-			(*m).GetStats(chi.URLParam(r, "name"), any(storage.Counter(value)), mType)
-			w.WriteHeader(http.StatusOK)
-		} else {
-			w.WriteHeader(http.StatusNotImplemented)
-			return
-		}
-	}
-}
-
-func GetMetricList(MetricList *map[string]storage.Gauge, CounterList *map[string]storage.Counter) func(w http.ResponseWriter, r *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		url := r.URL.Path
-		data := strings.Split(url, "/")
-
-		if data[2] == "gauge" {
-			if len(data) > 4 {
-				value, err := strconv.ParseFloat(data[4], 64)
-				if err != nil {
-					w.WriteHeader(http.StatusBadRequest)
-				}
-				(*MetricList)[data[3]] = storage.Gauge(value)
-				log.Println(MetricList)
-
-				w.WriteHeader(http.StatusOK)
-				return
-			}
-
-			w.WriteHeader(http.StatusNotFound)
-			return
-
-		}
-		if data[2] == "counter" {
-			if len(data) > 4 {
-				value, err := strconv.Atoi(data[4])
-				if err != nil {
-					w.WriteHeader(http.StatusBadRequest)
-				}
-				(*CounterList)[data[3]] = storage.Counter(value)
-				log.Println(CounterList)
-
-				w.WriteHeader(http.StatusOK)
-				return
-			}
-			w.WriteHeader(http.StatusNotFound)
-			return
-
-		}
-		w.WriteHeader(http.StatusNotImplemented)
-		return
-	}
 }
