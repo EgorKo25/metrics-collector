64b3b4e3a43387a1001e8798f67f072f181904be
Egor <egorko25@gmail.com>
2023-01-06 02:08:52 +0300

    Залогировал ошибку

--- internal/hashing/hashing.go
+++ internal/hashing/hashing.go
@@ -5,6 +5,7 @@ import (
 	"crypto/sha256"
 	"fmt"
 	"github.com/EgorKo25/DevOps-Track-Yandex/internal/storage"
+	"log"
 )
 
 type Hash struct {
@@ -21,12 +22,7 @@ func (h *Hash) Run(metric *storage.Metric) (hash string, err error) {
 
 	var src []byte
 
-	if metric.MType == "gauge" {
-		src = []byte(fmt.Sprintf("%s:guage:%f", metric.ID, *metric.Value))
-	}
-	if metric.MType == "counter" {
-		src = []byte(fmt.Sprintf("%s:counter:%d", metric.ID, *metric.Delta))
-	}
+	src = []byte(fmt.Sprintf("%s:%s:%f", metric.ID, metric.MType, *metric.Value))
 
 	if h.Key == nil {
 		return
@@ -36,6 +32,8 @@ func (h *Hash) Run(metric *storage.Metric) (hash string, err error) {
 	hm.Write(src)
 	hash = fmt.Sprintf("%x", hm.Sum(nil))
 
+	log.Println("Вычисленный ", hash)
+	log.Println("Имеющийся ", metric.Hash)
 	if metric.Hash != "" && !hmac.Equal([]byte(metric.Hash), []byte(hash)) {
 		return "", fmt.Errorf("not equal hash")
 	}
